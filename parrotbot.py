78568b2bf81335f751e4de9d63bec7e896583c34
Ellie Clifford <git@clifford.lol>
2021-03-27 03:01:00 +0000

    Add /parrotcheckhealth

--- parrotbot.py
+++ parrotbot.py
@@ -5,10 +5,19 @@ import parrotmaker_slack
 import pong_slack
 import gdrive_slack
 
-if __name__ == "__main__":
-    SocketModeHandler(app, open("SLACK_APP_TOKEN").read()).start()
+@app.command("/parrotcheckhealth")
+def parrotcheckhealth(client, ack, body, say):
+    say(f"I'm running! Here is my terminal output: \n"
+        "stdout\n```\n"
+      + open("log_stdout").read()
+      + "```\nstderr\n```\n"
+      + open("log_stderr").read()
+      + "```")
+    ack()
 
+if __name__ == "__main__":
     for chan in app.client.conversations_list()['channels']:
         if not (chan['is_im'] or chan['is_member'] or chan['is_archived']):
             app.client.conversations_join(channel=chan['id'])
 
+    SocketModeHandler(app, open("SLACK_APP_TOKEN").read()).start()
