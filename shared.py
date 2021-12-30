54883ca28da5246bd2a79861451064162cc48f3e
andriii25 <foldes.andris@gmail.com>
2021-12-30 19:47:44 +0100

    Added config stuff to shared.py as well
    Co-authored-by: Tim Clifford <tclifford@protonmail.com>

--- shared.py
+++ shared.py
@@ -1,2 +1,6 @@
+import yaml
+
 from slack_bolt import App
+
 app = App(token = open("/opt/slack-parrotbot/secrets/SLACK_BOT_TOKEN").read())
+config = yaml.load(open("config.yaml"), Loader=yaml.FullLoader)
