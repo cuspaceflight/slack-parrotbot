c14305594841c37ea32f85587ad2cbd26c29eb49
Tim Clifford <tclifford@protonmail.com>
2022-01-01 17:57:32 +0000

    Fix alignment, indentation, and gdrive

--- gdrive.py
+++ gdrive.py
@@ -1,43 +1,42 @@
 import os
 from threading import Thread
-from datetime import datetime
 
 from shared import app, config
 
 @app.event("file_shared")
 def handle_file_shared(client, event, say, ack):
-    file_data = client.files_info(file = event["file_id"]).data["file"]
-    user_data = client.users_info(user = event["user_id"]).data["user"]
-    print(datetime.now().isoformat(), ": File shared by ",
-          user_data['real_name'], flush=True)
+	file_data = client.files_info(file = event["file_id"]).data["file"]
+	user_data = client.users_info(user = event["user_id"]).data["user"]
+	print(datetime.now().isoformat(), ": File shared by ",
+		  user_data['real_name'], flush=True)
 
-    normalised_name = user_data['real_name'].replace(" ", "_")
-    dir_path = f"{config['gdrive']['local_path']/{normalised_name}"
+	normalised_name = user_data['real_name'].replace(" ", "_")
+	dir_path = f"{config['gdrive']['local_path']}/{normalised_name}"
+	dir_nice_name = f"{config['gdrive']['remote_nice_name']}/{normalised_name}"
 
-    msg_data = say(f"File uploading to "
-                   f"{remote_nice_name}/{normalised_name}...").data
-    Thread(target  = download_file,
-           args    = (client, dir_path, file_data, msg_data)).start()
-    ack()
+	msg_data = say(f"File uploading to {dir_nice_name}...").data
+	Thread(target  = download_file,
+	       args    = (client, dir_path, dir_nice_name, file_data, msg_data)).start()
+	ack()
 
-def download_file(client, dir_path, file_data, msg_data):
-    # wrap in sh -c in case the system shell is something stupid and non-posix
-    os.system(f"""sh -c '
+def download_file(client, dir_path, dir_nice_name, file_data, msg_data):
+	# wrap in sh -c in case the system shell is something stupid and non-posix
+	os.system(f"""sh -c '
 
-        mkdir -p "{dir_path}"
-        cd "{dir_path}"
+		mkdir -p "{dir_path}"
+		cd "{dir_path}"
 
-        wget --header="Authorization: Bearer {config['slack_bot_token']}"\
-            {file_data["url_private_download"]}
+		wget --header="Authorization: Bearer {config['slack_bot_token']}"\
+			{file_data["url_private_download"]}
 
-        cd - >/dev/null
+		cd - >/dev/null
 
-        # wait for the last operation to finish
-        while pgrep rclone; do sleep 1; done
-        rclone sync {config['gdrive']['local_path']} {config['gdrive']['remote_path']}
-    '""")
-    client.chat_update(
-            channel  = msg_data['channel'],
-            ts       = msg_data['ts'],
-            text     = f"File uploaded to "
-                       f"{remote_nice_name}/{normalised_name}")
+		# wait for the last operation to finish
+		while pgrep rclone; do sleep 1; done
+		rclone sync {config['gdrive']['local_path']} {config['gdrive']['remote_path']}
+	'""")
+	client.chat_update(
+			channel  = msg_data['channel'],
+			ts       = msg_data['ts'],
+			text     = f"File uploaded to {dir_nice_name}"
+		)
