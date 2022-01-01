76c10c656cae0bc44b633b68a6d367a476052adc
Ellie Clifford <git@clifford.lol>
2022-01-01 17:57:32 +0000

    Fix alignment, indentation, and gdrive

--- parrotmaker.py
+++ parrotmaker.py
@@ -4,18 +4,18 @@ from util.parrotmaker import ParrotMaker
 from shared import app
 
 pmaker = ParrotMaker(
-    fg         = ':fireparrot:',
-    bg         = ':hunt:',
-    max_width  = 57,
+	fg         = ':fireparrot:',
+	bg         = ':hunt:',
+	max_width  = 57,
 )
 
 @app.command("/parrot")
 def parrot(client, ack, body, say):
-    try:
-        say(f"<@{body['user_id']}> has summoned the parrot gods, "
-            f"and in response they say")
-        say(pmaker.to_parrots(body['text']))
-        ack()
-    except Exception as e:
-        response = str(e)
-        ack(response)
+	try:
+		say(f"<@{body['user_id']}> has summoned the parrot gods, "
+			f"and in response they say")
+		say(pmaker.to_parrots(body['text']))
+		ack()
+	except Exception as e:
+		response = str(e)
+		ack(response)
