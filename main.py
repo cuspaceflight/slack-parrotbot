6fe7f3f00d64253b9f961e6b23593fd113e6de5e
andriii25 <foldes.andris@gmail.com>
2024-08-30 18:07:47 +0200

    First move to async Slack API

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from datetime import datetime
-from subprocess import check_output
-
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
 
 from shared import *
 
-for feat in config['features_enabled']:
-	exec(f"import {feat}")
+if config['features_enabled'] is not None:
+	for feat in config['features_enabled']:
+		exec(f"import {feat}")
 
 print("Starting parrotbot...", flush=True, file=info_stream)
 
-@app.command("/parrotcheckhealth")
-def parrotcheckhealth(client, ack, body, say):
+
+@slack_app.command("/parrotcheckhealth")
+async def parrotcheckhealth(client, ack, body, say):
+	user_id = body["user_id"]
+	await ack(f"Hi <@{user_id}>!")
 	MAX_CHARS=2500
 
-	log = check_output(config['log']['command'], shell=True, text=True).replace('files.slack.com', '********')
+	log = await async_check_output(config['log']['command'], shell=True)
+	log = log.decode().replace('files.slack.com', '********')
 	log_lines = log.split('\n')
 	log_messages = []
-
 	# incredibly jank but whatever
 	while len(log_lines) > 0:
 		msg = ""
@@ -26,24 +27,28 @@ def parrotcheckhealth(client, ack, body, say):
 		log_messages.append(msg)
 
 	if "quiet" in body['text']:
-		ack("I'm running! Here is my latest log:" \
+		await ack("I'm running! Here is my latest log:" \
 		    "\n```\n" + log_messages[-1] + '```')
 	else:
-		say("I'm running! Here is my log:")
+		await say("I'm running! Here is my log:")
 		for msg in log_messages:
-			say("\n```\n" + msg + '```', unfurl_media = False, unfurl_links=False)
-		ack()
+			await say("\n```\n" + msg + '```', unfurl_media = False, unfurl_links=False)
+		await ack()
 
 
-if __name__ == "__main__":
+async def main():
 	# why no do while python??
 	cursor = None
 	while cursor != '':
-		conversations = app.client.conversations_list(cursor=cursor)
+		conversations = await slack_app.client.conversations_list(cursor=cursor)
 		for chan in conversations['channels']:
 			if not (chan['is_im'] or chan['is_member'] or chan['is_archived']):
-				app.client.conversations_join(channel=chan['id'])
+				await slack_app.client.conversations_join(channel=chan['id'])
 				print(f"Joined {chan['name']}", flush=True, file=info_stream)
 		cursor = conversations['response_metadata']['next_cursor']
+	slack_handler = AsyncSocketModeHandler(slack_app, config['slack_app_token'])
+	await slack_handler.start_async()
 
-	SocketModeHandler(app, config['slack_app_token']).start()
+if __name__ == "__main__":
+	import asyncio
+	asyncio.run(main())
--- shared.py
+++ shared.py
@@ -1,7 +1,10 @@
-import yaml
+import asyncio
 import sys
 import argparse
-from slack_bolt import App
+
+import yaml
+from slack_bolt.app.async_app import AsyncApp
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--journald', action='store_true',
                     help='Use systemd journal outputs instead of stdout and stderr')
@@ -9,6 +12,21 @@ parser.add_argument('-c', '--config',
                     help='Specify a path for a config.yaml',
                     default='/etc/slack-parrotbot/config.yaml')
 args = parser.parse_args()
+
+async def async_check_output(cmd, **kwargs):
+	process = await asyncio.create_subprocess_shell(
+		cmd,
+		stdout=asyncio.subprocess.PIPE,
+		stderr=asyncio.subprocess.PIPE,
+		**kwargs
+	)
+	stdout, stderr = await process.communicate()
+	if process.returncode == 0:
+		return stdout
+	else:
+		print(f"Error occured while executing command "
+			  f"`{cmd}` during /parrotcheckhealth:", file=err_stream)
+		print(stderr, file=err_stream)
 
 if args.journald:
 	from systemd import journal
@@ -38,4 +56,4 @@ else:
 
 config = yaml.load(open(config_file), Loader=yaml.loader.FullLoader)
 
-app = App(token = config['slack_bot_token'])
+slack_app = AsyncApp(token = config['slack_bot_token'])
