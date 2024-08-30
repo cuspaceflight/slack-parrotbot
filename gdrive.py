5ca1f8490ef0f10e654112740d8a5dfe86737a62
andriii25 <foldes.andris@gmail.com>
2024-08-30 18:26:04 +0200

    Re-implementing the gdrive sync & replaced shell execution with subprocesses and aiohttp

--- gdrive.py
+++ gdrive.py
@@ -1,42 +1,52 @@
-import os
-from threading import Thread
+from pathlib import Path
+import aiohttp
 
 from shared import *
 
-@app.event("file_shared")
-def handle_file_shared(client, event, say, ack):
-	file_data = client.files_info(file = event["file_id"]).data["file"]
-	user_data = client.users_info(user = event["user_id"]).data["user"]
+@slack_app.event("file_shared")
+async def handle_file_shared(client, event, say, ack):
+	await ack()
+	file_data = (await client.files_info(file = event["file_id"])).data["file"]
+	user_data = (await client.users_info(user = event["user_id"])).data["user"]
 	print("File shared by ", user_data['real_name'], flush=True, file=info_stream)
 
 	normalised_name = user_data['real_name'].replace(" ", "_")
-	dir_path = f"{config['gdrive']['local_path']}/{normalised_name}"
+	dir_path = Path(f"{config['gdrive']['local_path']}") / f"{normalised_name}"
 	dir_nice_name = f"{config['gdrive']['remote_nice_name']}/{normalised_name}"
 
-	msg_data = say(f"File uploading to {dir_nice_name}...").data
-	Thread(target  = download_file,
-	       args    = (client, dir_path, dir_nice_name, file_data, msg_data)).start()
-	ack()
+	msg_data = (await say(f"File uploading to {dir_nice_name}...")).data
 
-def download_file(client, dir_path, dir_nice_name, file_data, msg_data):
-	# wrap in sh -c in case the system shell is something stupid and non-posix
-	os.system(f"""sh -c '
+	await download_file(dir_path, file_data)
+	rclone_log = Path(config['gdrive']['rclone_log_path'])
+	rclone_log.parent.mkdir(exist_ok=True, parents=True)
+	with open(rclone_log, 'a+') as rclone_log_file:
+		process = await asyncio.create_subprocess_exec('rclone',
+													   'sync', config['gdrive']['local_path'], config['gdrive']['remote_path'],
+													   stdout=rclone_log_file, stderr=rclone_log_file)
+	await process.wait()
+	if process.returncode != 0:
+		print(f"Error code {process.returncode} occured during uploading to the remote via rclone. "
+			  f"See {str(rclone_log)} for details.", file=err_stream)
 
-		mkdir -p "{dir_path}"
-		cd "{dir_path}"
+	await client.chat_update(
+		channel  = msg_data['channel'],
+		ts       = msg_data['ts'],
+		text     = f"File uploaded to {dir_nice_name}" if process.returncode == 0 else f"File *failed* to upload to {dir_nice_name}"
+	)
 
-		wget --header="Authorization: Bearer {config['slack_bot_token']}"\
-			{file_data["url_private_download"]} >> /var/log/slack-parrotbot/wget-log
 
-		cd - >/dev/null
+async def download_file(dir_path, file_data):
+	header = {
+		"Authorization": f"Bearer {config['slack_bot_token']}",
+	}
+	dir_path.mkdir(exist_ok=True, parents=True)
+	async with aiohttp.ClientSession(headers=header) as session:
+		async with session.get(file_data['url_private_download']) as resp:
+			if resp.status == 200:
+				with open(dir_path / file_data['name'], 'wb') as f:
+					async for chunk in resp.content.iter_chunked(4096):
+						f.write(chunk)
 
-		# wait for the last operation to finish
-		while pgrep rclone; do sleep 1; done
-		rclone sync {config['gdrive']['local_path']} {config['gdrive']['remote_path']} \
-				>> /var/log/slack-parrotbot/rclone-log
-	'""")
-	client.chat_update(
-			channel  = msg_data['channel'],
-			ts       = msg_data['ts'],
-			text     = f"File uploaded to {dir_nice_name}"
-		)
+			else:
+				print(f"Received non-200 response when downloading file "
+					  f"{file_data['url_private_download']}", file=err_stream)
