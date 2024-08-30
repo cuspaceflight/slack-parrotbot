5fa6d349a9c5f2d0b1709a9e5b1ce0d89e2725e8
andriii25 <foldes.andris@gmail.com>
2024-08-30 18:29:32 +0200

    Re-done the live archive with async API & fixed locking to apply to all file accesses

--- live_archive.py
+++ live_archive.py
@@ -1,8 +1,7 @@
 import json
-import sys
 from datetime import datetime
 from pathlib import Path
-from threading import Lock, get_ident
+import aiofiles
 
 from shared import *
 
@@ -19,23 +18,27 @@ def timestamp_to_date_string(ts):
 		raise TypeError("Expected either float or string timestamp")
 
 
-def channel_id_to_name(channel_id):
+async def channel_id_to_name(channel_id):
 	"""Gets the current name of the channel with ID 'channel_id'"""
-	res = app.client.conversations_info(channel=channel_id)
+	res = await slack_app.client.conversations_info(channel=channel_id)
 	if "ok" not in res.data or not res["ok"]:
 		print(f"Error message:{res}", file=err_stream, flush=True)
 		raise ConnectionError(f"conversations_info(channel={channel_id}) web request failed")
 	channel_name = res["channel"]["name"]
 	return channel_name
 
+def create_lock(file_path):
+	if file_path not in file_locks:
+		file_locks[file_path] = asyncio.Lock()
 
-def log_file_path(channel_id, ts):
-	"""Returns a Path object pointing to the JSON logfile of a message with timestamp 'ts' in channel 'channel_id"""
-	channel_name = channel_id_to_name(channel_id)
+
+async def log_file_path(channel_id, ts):
+	"""Returns a Path object pointing to the JSON logfile of a message with timestamp 'ts' in channel 'channel_id'"""
+	channel_name = await channel_id_to_name(channel_id)
 	return archive_path / channel_name / (timestamp_to_date_string(ts) + ".json")
 
-@app.event("message")
-def archive_message(message):
+@slack_app.event("message")
+async def archive_message(message):
 	"""Runs on every message event and archives it if it's not hidden"""
 	print("Message received", flush=True, file=debug_stream)
 
@@ -43,30 +46,39 @@ def archive_message(message):
 	# Otherwise the program would still need to handle it to put it in the correct folder
 	if "subtype" in message:
 		if message['subtype'] == "channel_name":
-			rename_channel(message['channel'], message['old_name'], message['name'])
+			await rename_channel(message['channel'], message['old_name'], message['name'])
 		elif message['subtype'] ==  "message_changed":
-			update_message(message['channel'], message['message']['ts'], message['message'])
+			await update_message(message['channel'], message['message']['ts'], message['message'])
 
 	# Thread reply, need to edit parent message
 	# Cannot filter on subtype as no subtype field is omitted due to bug in Slack API
 	# See here: https://api.slack.com/events/message/message_replied
 	if "thread_ts" in message:
-		add_thread_reply(message['channel'], message['thread_ts'],
+		await add_thread_reply(message['channel'], message['thread_ts'],
 						 message['user'], message['ts'])
 
 	if "hidden" in message:
 		if not message['hidden']:
-			add_to_archive(message)
+			await add_to_archive(message)
 			# This probably should not ever occur,
 			# because if the message has a 'hidden' field it's probably always true
 	else:
-		add_to_archive(message)
+		await add_to_archive(message)
 
 
-def update_message(channel_id, ts, updated_message):
+async def update_message(channel_id, ts, updated_message):
 	"""Replaces message with timestamp 'ts' in channel 'channel_id' with updated message"""
-	with open(log_file_path(channel_id, ts), 'r+') as log_file:
-		message_list = json.load(log_file)
+	log_path = await log_file_path(channel_id, ts)
+	create_lock(log_path)
+	print(f"Attempting to acquire file lock for {log_path}",
+		  file=debug_stream, flush=True)
+	async with file_locks[log_path]:
+		print(f"Acquired file lock for {log_path}",
+			  file=debug_stream, flush=True)
+		async with aiofiles.open(log_path, "r") as log_file:
+			log_string = await log_file.read()
+
+		message_list = json.loads(log_string) # No async way of doing this afaik
 		for message in message_list:
 			# Update fields of message
 			# This is done via iteration instead of a complete replacement
@@ -74,16 +86,26 @@ def update_message(channel_id, ts, updated_message):
 			if message['ts'] == ts:
 				for (key, value) in updated_message.items():
 					message[key] = value
+		log_string = json.dumps(message_list, indent=4)
 
-		log_file.seek(0)
-		json.dump(message_list, log_file, indent=4)
-		log_file.truncate()
+		async with aiofiles.open(log_path, "w") as log_file:
+			await log_file.write(log_string)
+	print(f"Released file lock for {log_path}",
+		  file=debug_stream, flush=True)
 
-
-def add_thread_reply(channel_id, thread_ts, reply_user, reply_ts):
+async def add_thread_reply(channel_id, thread_ts, reply_user, reply_ts):
 	"""Adds a thread reply object to an archived message"""
-	with open(log_file_path(channel_id, thread_ts), 'r+') as log_file:
-		message_list = json.load(log_file)
+	log_path = await log_file_path(channel_id, thread_ts)
+	create_lock(log_path)
+	print(f"Attempting to acquire file lock for {log_path}",
+		  file=debug_stream, flush=True)
+	async with file_locks[log_path]:
+		print(f"Attempting to acquire file lock for {log_path}",
+			  file=debug_stream, flush=True)
+		async with aiofiles.open(log_path, 'r') as log_file:
+			message_string = await log_file.read()
+
+		message_list = json.loads(message_string)
 		message = next((m for m in message_list if m['ts'] == thread_ts), None)
 		if message is not None:
 			if "reply_count" in message:
@@ -111,56 +133,67 @@ def add_thread_reply(channel_id, thread_ts, reply_user, reply_ts):
 				# Though not sure how relevant this is, as slack-export-viewer does not seem to care
 		else:
 			print(f"Received reply to thread (channel={channel_id}, ts={reply_ts}) but have not found"
-			      f"original thread with ts = {thread_ts}.", file=err_stream, flush=True)
-		# Go back to start and write
-		log_file.seek(0)
-		json.dump(message_list, log_file, indent=4)
-		log_file.truncate()
+				  f"original thread with ts = {thread_ts}.", file=err_stream, flush=True)
+		log_string = json.dumps(message_list, indent=4)
+		async with aiofiles.open(log_path, "w") as log_file:
+			await log_file.write(log_string)
+	print(f"Released file lock for {log_path}",
+		  file=debug_stream, flush=True)
 
 
-def rename_channel(channel_id, old_name, new_name):
+
+async def rename_channel(channel_id, old_name, new_name):
 	"""Renames channel folders and updates channel.json"""
 	channel_list_path = archive_path / "channels.json"
-
-	with open(channel_list_path, 'r+') as channel_list:
-		old_channel_list = json.load(channel_list)
+	create_lock(channel_list_path)
+	print(f"Attempting to acquire file lock for {channel_list_path}",
+		  file=debug_stream, flush=True)
+	async with file_locks[channel_list_path]:
+		print(f"Acquired file lock for {channel_list_path}",
+			  file=debug_stream, flush=True)
+		async with aiofiles.open(channel_list_path, 'r') as channel_list:
+			old_channel_list_string = await channel_list.read()
+		old_channel_list = json.loads(old_channel_list_string)
 		channel = next((ch for ch in old_channel_list if ch['id'] == channel_id), None)
 		if channel is not None:
 			channel['name'] = new_name
 		else:
 			print(f"Renamed channel (id={channel_id}) from {old_name} to {new_name},"
-			      f"but have not found channel with id on channels.json",
-			      file=err_stream, flush=True)
-		channel_list.seek(0)
-		json.dump(old_channel_list, channel_list, indent=4)
-		channel_list.truncate()
+				  f"but have not found channel with id on channels.json",
+				  file=err_stream, flush=True)
 
-	print(f"Channel rename event from {old_name} to {new_name}", flush=True,
-			file=info_stream)
-	if old_name is not None:
-		old_path = archive_path / old_name
-		new_path = archive_path / new_name
-		old_path.rename(new_path)
-	else:
-		print(f"Attempted to rename channel id {channel_id}, but it doesn't exist",
-		      flush=True, file=warn_stream)
+		old_channel_list_string = json.dumps(old_channel_list, indent=4)
+		async with aiofiles.open(channel_list_path, "w") as channel_list:
+			await channel_list.write(old_channel_list_string)
+
+		print(f"Channel rename event from {old_name} to {new_name}", flush=True,
+				file=info_stream)
+		if old_name is not None:
+			old_path = archive_path / old_name
+			new_path = archive_path / new_name
+			old_path.rename(new_path)
+		else:
+			print(f"Attempted to rename channel id {channel_id}, but it doesn't exist",
+				  flush=True, file=warn_stream)
+	print(f"Released file lock for {channel_list_path}",
+		  file=debug_stream, flush=True)
 
 
-@app.event("channel_created")
-def create_channel(client, payload):
+@slack_app.event("channel_created")
+async def create_channel(client, payload):
 	"""On channel creation, if it's an actual channel joins then adds it into channels.json"""
 	channel = payload['channel']
 	channel_id = channel['id']
 
 	# Get all detailed channel object
-	res = client.conversations_info(channel=channel_id)
+	res = await client.conversations_info(channel=channel_id)
 	if "ok" not in res.data or not res['ok']:
 		print(f"Error message: {res}", file=err_stream, flush=True)
 		raise ConnectionError(f"Could not get channel info with id = {channel_id}")
 	full_channel_info = res["channel"]
 	if full_channel_info['is_channel']:
 		# Join channel if created
-		res = client.conversations_join(channel=channel_id)
+		res = await client.conversations_join(channel=channel_id)
 		if "ok" not in res.data or not res['ok']:
 			print(f"Error message: {res}", file=err_stream, flush=True)
 			raise ConnectionError(f"Could not join channel with id = {channel_id}")
@@ -169,16 +202,26 @@ def create_channel(client, payload):
 		      file=info_stream)
 
 		channel_list_path = archive_path / "channels.json"
-		with open(channel_list_path, 'r+') as channel_list:
-			old_channel_list = json.load(channel_list)
-			old_channel_list.append(full_channel_info)
-			channel_list.seek(0)
-			json.dump(old_channel_list, channel_list, indent=4)
-			channel_list.truncate()
+		create_lock(channel_list_path)
+		print(f"Attempting to acquire file lock for {channel_list_path}",
+			  file=debug_stream, flush=True)
+		async with file_locks[channel_list_path]:
+			print(f"Acquired file lock for {channel_list_path}",
+				  file=debug_stream, flush=True)
+			async with aiofiles.open(channel_list_path, 'r+') as channel_list:
+				old_channel_list_string = await channel_list.read()
+				old_channel_list = json.loads(old_channel_list_string)
+				old_channel_list.append(full_channel_info)
+				await channel_list.seek(0)
+				old_channel_list_string = json.dumps(old_channel_list, indent=4)
+				await channel_list.write(old_channel_list_string)
+				await channel_list.truncate()
+		print(f"Released file lock for {channel_list_path}",
+			  file=debug_stream, flush=True)
 
 
-@app.event("reaction_added")
-def add_reaction(payload):
+@slack_app.event("reaction_added")
+async def add_reaction(payload):
 	"""Adds reaction to the archived message"""
 	reaction = payload['reaction']
 	reacting_user = payload['user']
@@ -188,8 +231,17 @@ def add_reaction(payload):
 		parent_channel_id = payload['item']['channel']
 		parent_ts = payload['item']['ts']
 
-		with open(log_file_path(parent_channel_id, parent_ts), "r+") as log_file:
-			message_list = json.load(log_file)
+		log_path = await log_file_path(parent_channel_id, parent_ts)
+		create_lock(log_path)
+		print(f"Attempting to acquire file lock for {log_path}",
+			  file=debug_stream, flush=True)
+		async with file_locks[log_path]:
+			print(f"Acquired file lock for {log_path}",
+				  file=debug_stream, flush=True)
+			async with aiofiles.open(log_path, "r") as log_file:
+				message_list_string = await log_file.read()
+
+			message_list = json.loads(message_list_string)
 			message = next((m for m in message_list if m['ts'] == parent_ts), None)
 			if message is not None:
 				new_reaction_entry = {'name': reaction, 'count': 1, 'users': [reacting_user]}
@@ -206,16 +258,18 @@ def add_reaction(payload):
 					message['reactions'] = [new_reaction_entry]
 			else:
 				print(f"Reaction {reaction} added to message (ts={parent_ts}, channel={parent_channel_id}), "
-				      f"but message not found in log. Ignoring...",
-				      file=err_stream, flush=True)
+					  f"but message not found in log. Ignoring...",
+					  file=err_stream, flush=True)
 
-			log_file.seek(0)
-			json.dump(message_list, log_file, indent=4)
-			log_file.truncate()
+			message_list_string = json.dumps(message_list, indent=4)
+			async with aiofiles.open(log_path, "w") as log_file:
+				await log_file.write(message_list_string)
+		print(f"Released file lock for {log_path}",
+			  file=debug_stream, flush=True)
 
 
-@app.event("reaction_removed")
-def remove_reaction(payload):
+@slack_app.event("reaction_removed")
+async def remove_reaction(payload):
 	"""Removes reaction from an archived message"""
 	reaction = payload['reaction']
 	reacting_user = payload['user']
@@ -225,8 +279,17 @@ def remove_reaction(payload):
 		parent_channel_id = payload['item']['channel']
 		parent_ts = payload['item']['ts']
 
-		with open(log_file_path(parent_channel_id, parent_ts), "r+") as log_file:
-			message_list = json.load(log_file)
+		log_path = await log_file_path(parent_channel_id, parent_ts)
+		create_lock(log_path)
+		print(f"Attempting to acquire file lock for {log_path}",
+			  file=debug_stream, flush=True)
+		async with file_locks[log_path]:
+			print(f"Acquired file lock for {log_path}",
+				  file=debug_stream, flush=True)
+			async with aiofiles.open(log_path, "r+") as log_file:
+				message_list_string  = await log_file.read()
+
+			message_list = json.loads(message_list_string)
 			message = next((m for m in message_list if m['ts'] == parent_ts), None)
 			if message is not None:
 				if "reactions" in message:
@@ -242,32 +305,33 @@ def remove_reaction(payload):
 								message.pop('reactions')
 					else:
 						print(f"Reaction {reaction} removed from message (ts={parent_ts}, channel={parent_channel_id}), "
-						      f"but not in message's reaction list. Ignoring...",
-						      file=err_stream, flush=True)
+							  f"but not in message's reaction list. Ignoring...",
+							  file=err_stream, flush=True)
 				else:
 					print(f"Reaction {reaction} removed from message (ts={parent_ts}, channel={parent_channel_id}), "
-					      f"but message does not have reactions in log. Ignoring...",
-					      file=err_stream, flush=True)
+						  f"but message does not have reactions in log. Ignoring...",
+						  file=err_stream, flush=True)
 			else:
 				print(f"Reaction {reaction} removed from message (ts={parent_ts}, channel={parent_channel_id}), "
-				      f"but message not found. Ignoring...",
-				      file=err_stream, flush=True)
+					  f"but message not found. Ignoring...",
+					  file=err_stream, flush=True)
 
-			log_file.seek(0)
-			json.dump(message_list, log_file, indent=4)
-			log_file.truncate()
+			message_list_string = json.dumps(message_list, indent=4)
+			async with aiofiles.open(log_path, "w") as log_file:
+				await log_file.write(message_list_string)
+		print(f"Released file lock for {log_path}",
+			  file=debug_stream, flush=True)
 
-
-def add_to_archive(message):
+async def add_to_archive(message):
 	"""Archives a message"""
 
 	channel_id = message['channel']
 	ts = message['ts']
 
-	channel_name = channel_id_to_name(channel_id)
+	channel_name = await channel_id_to_name(channel_id)
 	directory_path = archive_path / channel_name
 	directory_path.mkdir(exist_ok=True, parents=True)
-	current_day_path = log_file_path(channel_id, ts)
+	current_day_path = await log_file_path(channel_id, ts)
 
 	# Reading an entire day's messages and rewriting is incredibly lazy and inefficient
 	# However it will do for now
@@ -275,38 +339,36 @@ def add_to_archive(message):
 	# A) manually remove last ] in JSON file with file.seek() and append last message object and add ] again
 	# B) manage an internal Archive object which writes to disk periodically (say at the end of each day / every 100 message)
 	# B seems to be preferable at the moment to me
+	# This does not change with asyncio really, B is still preferable but the lazy method worked fine so far
+	# and it will not be used a lot anyway from now on
 
-	print(f"Attempting to acquire file lock for {current_day_path}, thread id = {get_ident()}",
+	print(f"Attempting to acquire file lock for {current_day_path}",
 			file=debug_stream, flush=True)
 
 	# Lock file in case other threads try to write data to it at the same time
-	if current_day_path in file_locks:
-		file_locks[current_day_path].acquire()
-	else:
-		file_locks[current_day_path] = Lock()
-		file_locks[current_day_path].acquire()
 
-	print(f"Acquired file lock for {current_day_path}, thread id = {get_ident()}",
-			file=debug_stream, flush=True)
+	create_lock(current_day_path)
 
-	# Create file if it does not exist
-	if not current_day_path.is_file():
-		with open(current_day_path, "w") as current_day:
-			json.dump([message], current_day, indent=4)
+	async with file_locks[current_day_path]:
+		print(f"Acquired file lock for {current_day_path}",
+				file=debug_stream, flush=True)
 
-	else:
-		with open(current_day_path, 'r+') as current_day:
-			message_list = json.load(current_day)
+		# Create file if it does not exist
+		if not current_day_path.is_file():
+			async with aiofiles.open(current_day_path, "w") as current_day:
+				current_day_string = json.dumps([message], indent=4)
+				await current_day.write(current_day_string)
+
+		else:
+			async with aiofiles.open(current_day_path, 'r') as current_day:
+				message_list_string = await current_day.read()
+
+			message_list = json.loads(message_list_string)
 			message_list.append(message)
+			message_list_string = json.dumps(message_list, indent=4)
 
-			# Go back to start
-			current_day.seek(0)
+			async with aiofiles.open(current_day_path, "w") as current_day:
+				await current_day.write(message_list_string)
 
-			json.dump(message_list, current_day, indent=4)
-			current_day.truncate()
-
-	# Release file lock
-	file_locks[current_day_path].release()
-
-	print(f"Released file lock for {current_day_path}, thread id = {get_ident()}",
+	print(f"Released file lock for {current_day_path}",
 			file=debug_stream, flush=True)
